3317764ad78f72da0590d35cd307b8c0233a6189
Hank Lo <Hlo1453@gmail.com>
2016-02-16 19:38:13 -0800

    minor bug fix syntax errors

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -98,12 +98,12 @@ class Order extends Application {
 
     // proceed with checkout
     function commit($order_num) {
-        if (!this->order->validate($order_num))
+        if (!$this->order->validate($order_num))
             redirect('order/display_menu/', $order_num);
         $record = $this->orders->get($order_num);
         $record->date = date(DATE_ATOM);
         $record->status = 'c';
-        $record->total = $this->orders->total($or
+        $record->total = $this->orders->total($order_num);
         redirect('/');
     }
 
